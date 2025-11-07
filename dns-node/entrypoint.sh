3f864d2443370fb45e3220dc4df7b9672a719060
Darren Davison <darren@davisononline.org>
2025-11-07 18:22:24 +0000

    implement SINKHOLE_DEBUG for `dns-node`

--- dns-node/entrypoint.sh
+++ dns-node/entrypoint.sh
@@ -4,6 +4,12 @@ export LOCAL_DOMAIN=${LOCAL_DOMAIN:-_invalid.local}
 export LOCAL_UPSTREAM_DNS=${LOCAL_UPSTREAM_DNS:-8.8.4.4}
 export UPSTREAM_DNS=${UPSTREAM_DNS:-1.1.1.1 9.9.9.9}
 
+if [ "$SINKHOLE_DEBUG" == "true" ]; then
+    export CORE_RELOAD="reload 10s"
+else
+    export CORE_RELOAD="reload 1h"
+fi
+
 # 1. Run as ROOT: Substitute env vars and create final config
 envsubst < /etc/coredns/Corefile.template > /etc/coredns/Corefile
 
