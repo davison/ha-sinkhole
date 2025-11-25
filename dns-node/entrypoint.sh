79c008899e14b7100bfde0ad9f56cf7d8417deb1
Darren Davison <darren@davisononline.org>
2025-11-25 17:26:36 +0000

    fix(dns-node): change debug values
    - Remove `lamsduck` config which makes no sense without a `ready` check
    - Make blocklist TTL and cache duration vary with `SINKHOLE_DEBUG` env var

--- dns-node/entrypoint.sh
+++ dns-node/entrypoint.sh
@@ -6,9 +6,13 @@ export UPSTREAM_DNS=${UPSTREAM_DNS:-1.1.1.1 9.9.9.9}
 export TRUSTED_NETS=${TRUSTED_NETS:-192.168.0.0/16 172.16.0.0/12 10.0.0.0/8 169.254.1.2/32}
 
 if [ "$SINKHOLE_DEBUG" == "true" ]; then
-    export CORE_RELOAD="reload 10s"
+    export RELOAD_INTERVAL="10s"
+    export TTL_DURATION="5"
+    export CACHE_DURATION="10"
 else
-    export CORE_RELOAD="reload 1h"
+    export RELOAD_INTERVAL="10m"
+    export TTL_DURATION="900"
+    export CACHE_DURATION="3600"
 fi
 
 # 1. Run as ROOT: Substitute env vars and create final config
